c6a431066dd3f843077b48550d2623063b12111f
O'Brien Lab <60624495+obrien-lab-psu@users.noreply.github.com>
2022-05-15 22:32:38 -0400

    Delete automate_ref_ent_clustering directory

--- automate_ref_ent_clustering/example.cmd
+++
@@ -1,2 +0,0 @@
-#to analyze a series of single refernce state that were analyzed for the prescence of entanglement only use this command
-python automate_ent_clustering_v4.4.py inpfiles/6u32_CP\*.pkl 6u32_CP_total
